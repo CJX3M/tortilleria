1c5ac40db568788f6b5726b68c96df220c69d13d
Carlos López <cjlc@hotmail.com>
2018-10-22 16:00:48 -0700

    Algo

---
+++ src/app/api/index.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyBYRC3V8pvOduOgO58xqp-RTH8RM8LN01M",
+    authDomain: "tortilleria-35f9c.firebaseapp.com",
+    databaseURL: "https://tortilleria-35f9c.firebaseio.com",
+    projectId: "tortilleria-35f9c",
+    storageBucket: "tortilleria-35f9c.appspot.com",
+    messagingSenderId: "143484736391"
+  };
+  firebase.initializeApp(config);
+
+export class api {
+    
+}
